efc1ccefadc27ad36ef3bb1ede4c54ede608dc2b
soif <wxopwx@gmail.com>
2013-10-01 15:47:29 +0200

    Disable Cache + better formatting

--- example.php
+++ example.php
@@ -1,16 +1,23 @@
 <?php
+//Require Class ##############################################################
 include(dirname(__FILE).'/twitter2array.class.php');
 
-
+//INIT #######################################################################
 //$tw=new Twitter2array('json'); //use this if you want to output json
 $tw=new Twitter2array();
 
-// if you want to use cache , set these
-$cache_file	=dirname(__FILE).'/cache/'; // be sure to set this directory writable, ie chmod 777
-$cache_time	=3600; // store results for 1 hour
-$cache_mode ='out'; // 'out' = store result (faster) || 'in' = store fetched urls (usefull for debugging)
-$tw->SetCache($cache_file,$cache_time,$cache_mode);
 
+// CACHE (optionnal) #########################################################
+// if you want to use cache , set these correctly
+$cache_file	=dirname(__FILE).'/cache/'; // absolute path to your cache directory . Be sure to set this directory writable, ie chmod 777
+$cache_time	=3600; 						// store results for 1 hour
+$cache_mode ='out'; 					// 'out' = store results (faster) || 'in' = store fetched urls (usefull for debugging)
+
+// if you want to use cache , uncomment this
+//$tw->SetCache($cache_file,$cache_time,$cache_mode);
+
+
+// LET'S GO ################################################################
 
 $user_name1	="cosmocatalano";
 $user_name2	="obama";
